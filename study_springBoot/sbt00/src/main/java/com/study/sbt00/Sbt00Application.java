bed7c08a15d411634bdb1ebb2c18f2604668519c
jyeeeh <universe78a@gmail.com>
2024-01-12 13:47:23 +0900

    springboot Init

---
+++ study_springBoot/sbt00/src/main/java/com/study/sbt00/Sbt00Application.java
@@ -0,0 +1,13 @@
+package com.study.sbt00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbt00Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbt00Application.class, args);
+	}
+
+}
---
+++ study_springBoot/sbt00/src/test/java/com/study/sbt00/Sbt00ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.sbt00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbt00ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
